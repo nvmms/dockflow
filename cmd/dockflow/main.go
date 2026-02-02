695fe1d34278c276332f1891b074eacb16331073
8qip3l <8qip3l@163.com>
2026-02-02 16:02:56 +0000

    feat:添加docker监听服务

--- cmd/dockflow/main.go
+++ cmd/dockflow/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "dockflow/internal/cli"
+import (
+	"context"
+	"dockflow/internal/cli"
+	"dockflow/internal/service/docker"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "daemon" {
+		runDaemon()
+		return
+	}
+
+	runCLI()
+}
+
+func runCLI() {
 	cli.Execute()
+}
+
+func runDaemon() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go docker.ListenDockerEvents(ctx)
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT)
+
+	<-sig
+	cancel()
+	log.Println("dockflow daemon stopped")
 }
