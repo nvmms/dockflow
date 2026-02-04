1baba4f4abecd2c2b03ff2401309cd6a5ec93892
asdfasdf <asdfasdf@qq.com>
2026-02-04 18:45:42 +0800

    feat:ns 添加查找app方法

--- internal/domain/namespace.go
+++ internal/domain/namespace.go
@@ -1,6 +1,10 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	"github.com/samber/lo"
+)
 
 type Namespace struct {
 	Name      string         `json:"name"`
@@ -12,4 +16,10 @@ type Namespace struct {
 	Redis     []RedisSpec    `json:"redis"`
 	Database  []DatabaseSpec `json:"database"`
 	App       []AppSpec      `json:"app"`
+}
+
+func (n *Namespace) FindApp(appName string) (AppSpec, bool) {
+	return lo.Find(n.App, func(app AppSpec) bool {
+		return appName == app.Name
+	})
 }
